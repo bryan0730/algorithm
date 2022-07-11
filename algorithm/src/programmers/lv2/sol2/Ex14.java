21bd3d7532e3d51cfe0e9ecec662bb6d52a5c1c5
bryan0730 <ehdgk9166@naver.com>
2022-07-11 23:42:22 +0900

    07-11

--- algorithm/src/programmers/lv2/sol2/Ex14.java
+++ algorithm/src/programmers/lv2/sol2/Ex14.java
@@ -9,8 +9,6 @@ public class Ex14 {
 	//https://school.programmers.co.kr/learn/courses/30/lessons/17677
 	public static int solution(String str1, String str2) {
 		
-		int answer = 0;
-		
 		List<String> strList1 = new ArrayList<>();
 		List<String> strList2 = new ArrayList<>();
 		List<String> gyoZip = new ArrayList<>();
